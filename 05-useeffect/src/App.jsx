33b08841e5b204aa9bfe1f7adc3e6deac0c56fa4
Zijian-Wang <ryegogogo@gmail.com>
2023-01-19 15:44:24 +0800

    Learnt about how to render to outside components using Modals with Portals in react.

--- 05-useeffect/src/App.jsx
+++ 05-useeffect/src/App.jsx
@@ -1,8 +1,8 @@
-import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import Details from "./Details"
-import SearchParams from "./SearchParams"
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import DetailsErrorBoundary from './Details'
+import SearchParams from './SearchParams'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +21,7 @@ const App = () => {
           <Link to="/">Adopt Me!</Link>
         </header>
         <Routes>
-          <Route path="/details/:id" element={<Details />} />
+          <Route path="/details/:id" element={<DetailsErrorBoundary />} />
           <Route path="/" element={<SearchParams />} />
         </Routes>
       </QueryClientProvider>
@@ -29,6 +29,6 @@ const App = () => {
   )
 }
 
-const container = document.getElementById("root")
+const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(<App />)
--- 05-useeffect/src/Details.jsx
+++ 05-useeffect/src/Details.jsx
@@ -2,12 +2,17 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import fetchPet from './fetchPet'
 import Carousel from './Carousel'
+import ErrorBoundary from './ErrorBoundary'
+import Modal from './Modal'
+import { useState } from 'react'
 
 const Details = () => {
   // Pulls the id from router
   const { id } = useParams()
   // Fetches the pet from the API using useQuery
   const results = useQuery(['details', id], fetchPet)
+
+  const [showModal, setShowModal] = useState(false)
 
   if (results.isLoading) {
     return (
@@ -26,12 +31,34 @@ const Details = () => {
         <h1>{pet.name}</h1>
         <h2>
           {pet.animal} – {pet.breed} – {pet.city}, {pet.state}
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p> {pet.description} </p>
+          {/* Show Modal that renders to another div on App.js level */}
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}? </h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </h2>
       </div>
     </div>
   )
 }
 
-export default Details
+// this wrapping way can also catch errors happening in the Details component that did not get returned,
+//  like "if (results.isLoading)" lines, that are above the return statement
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  )
+}
+
+export default DetailsErrorBoundary
